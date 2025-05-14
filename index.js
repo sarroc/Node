3b8cab864b1b39f1dca1f55ce203e7d7f624467f
Sarah Rocco <sarahrocco90@gmail.com>
2025-05-14 21:16:59 +0200

    require cache exports the same value

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const counterObject = require('./myscript.js');
 
-console.log(counterObject.getCounter());
+console.log(counterObject.getCounter()); 
 counterObject.incrementCounter();
 console.log(counterObject.getCounter());
+
+const newCounterObject = require('./myscript. js'); 
+// file gets required only once, value gets thrown into the require cache and the same value gets exported again
+
+console.log(newCounterObject.getCounter());
